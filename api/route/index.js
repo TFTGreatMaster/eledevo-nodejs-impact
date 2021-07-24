85baa75cb5185e4954c441d10db5984b833cf750
Your Name <khongmuonroixa98@gmail.com>
2021-07-24 22:00:46 +0700

    impact

---
+++ api/route/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+
+const todo = require('../controller/todoController')
+
+router.route('/items').get(todo.get_all_item).post(todo.post_a_item)
+
+router.route('/items/:id').delete(todo.del_a_item).put(todo.put_a_item)
+
+router.route('/items/pagi').get(todo.pagi_item)
+router.route('/items/search').get(todo.pagi_search_item)
+router.route('/items/search/id').get(todo.search_a_item)
+
+module.exports = router
